ed192cf5b7c55fd87b15fa985e84a44b4d0ace94
shubhangiwavhal <shubhangiwavhal22@gmail.com>
2021-11-23 15:26:44 +0530

    My First Git

---
+++ src/Gitwork/Hello.java
@@ -0,0 +1,9 @@
+package Gitwork;
+
+public class Hello {
+	public static void main(String args[])
+	{
+		System.out.println("Hello This is my first Git");
+		System.out.println("My first Git");
+	}
+}
